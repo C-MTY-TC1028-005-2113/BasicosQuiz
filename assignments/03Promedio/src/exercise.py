cb1446eab12b52723131e7fced8387f25d8402a3
roque63 <roque@itesm.mx>
2021-08-15 21:35:34 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #escribe tu código abajo de esta línea
     # importar el módulo math para tener las funciones disponibles
     import math
-
+    # para usar la raiz cuadrada debes llamar a la función math.sqrt( )
     pass
 
 if __name__ == '__main__':
